977f34d8a736ff875a94048e7a6940208155e4fe
user <Conquerorsword36@yahoo.com>
2023-12-05 17:32:23 -0500

    removed proxy and procfile

--- vite.config.js
+++ vite.config.js
@@ -11,21 +11,11 @@ dotenv.config();
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const { PORT = 3000 } = process.env;
-
 export default defineConfig({
   plugins: [react({
       jsxRuntime: 'classic' // Add this line
     }
   )],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-    },
-  },
   build: {
     manifest: true,
     rollupOptions: {
