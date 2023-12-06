749ce1fd650add27b43b3a857d94eb9df167292d
user <Conquerorsword36@yahoo.com>
2023-12-05 19:16:05 -0500

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})*/
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react({
-      jsxRuntime: 'classic' // Add this line
-    }
-  )],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.js'],
   },
 });
